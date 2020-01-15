30b7b37e5e38017f70540073e37da7bb101f70b1
vicabitu <victor.ch370@gmail.com>
2020-01-15 10:22:53 -0300

    Agrego end point para verificar validez del token

--- backend/app/generador_de_diplomas/urls.py
+++ backend/app/generador_de_diplomas/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
 ]
