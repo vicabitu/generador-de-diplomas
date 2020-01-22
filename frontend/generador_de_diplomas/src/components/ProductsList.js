9a4bb5c1f018a09f9ab899c47b8684f68bb094a5
vicabitu <victor.ch370@gmail.com>
2020-01-22 12:23:27 -0300

    Baja de producto desde el frontend

--- frontend/generador_de_diplomas/src/components/ProductsList.js
+++ frontend/generador_de_diplomas/src/components/ProductsList.js
@@ -4,6 +4,12 @@ import MaterialTable from 'material-table';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
 
 class ProductsList extends React.Component {
 
@@ -12,9 +18,12 @@ class ProductsList extends React.Component {
     this.state = {
       products: [],
       openModal: false,
-      name: '',
+      product_code: null,
       id: null
     }
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
   }
 
   componentDidMount() {
@@ -28,6 +37,31 @@ class ProductsList extends React.Component {
     .catch(function (error) {
       console.log(error);
     })
+  }
+
+  handleOpenModal(product_code, id) {
+    this.setState({product_code: product_code});
+    this.setState({id: id});
+    this.setState({openModal: true});
+  }
+
+  handleCloseModal() {
+    this.setState({openModal: false});
+  }
+
+  // Funcion que se encarga de manejar la confirmacion del modal.
+  // Elimina el producto.
+  handleConfirmModal() {
+    const url = 'http://127.0.0.1:8000/api/eliminar_producto/'+this.state.id;
+
+    axios.delete(url)
+    .then((response) => {
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    this.setState({openModal: false});
+    window.location.reload();
   }
 
   render() {
@@ -68,7 +102,7 @@ class ProductsList extends React.Component {
                 title: 'Eliminar',
                 field: 'delete',
                 render: rowData => (
-                  <IconButton aria-label="delete"> 
+                  <IconButton onClick={() => this.handleOpenModal(rowData.code, rowData.id)} aria-label="delete"> 
                     <DeleteIcon> 
                     </DeleteIcon>
                   </IconButton>
@@ -78,6 +112,34 @@ class ProductsList extends React.Component {
             data={this.state.products.length > 0 ? this.state.products : []}
           />
         </div>
+
+        <Dialog
+            open={this.state.openModal}
+            onClose={() => this.handleCloseModal()}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">Eliminar</DialogTitle>
+            <DialogContent id="alert-dialog-description">
+              <DialogContentText style={{display: 'inline'}}>
+                {'Desea eliminar el producto '}
+              </DialogContentText>
+              <DialogContentText style={{display: 'inline', fontWeight: 'bold'}}>
+                {this.state.product_code}
+              </DialogContentText>
+              <DialogContentText style={{display: 'inline'}}>
+                {'?'}
+              </DialogContentText>
+              <DialogActions>
+                <Button onClick={() => this.handleCloseModal()} color="primary">
+                  Cancelar
+                </Button>
+                <Button onClick={() => this.handleConfirmModal()} color="primary" autoFocus>
+                  Aceptar
+                </Button>
+              </DialogActions>
+            </DialogContent>
+          </Dialog>
       </div>
     )
   }
