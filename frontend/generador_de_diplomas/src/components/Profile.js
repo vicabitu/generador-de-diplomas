3107f58b6a48b784853cf4aef1f348042fd76d27
vicabitu <victor.ch370@gmail.com>
2020-01-25 13:18:25 -0300

    Campo de pais en la pantalla de edicion de perfil

--- frontend/generador_de_diplomas/src/components/Profile.js
+++ frontend/generador_de_diplomas/src/components/Profile.js
@@ -21,7 +21,8 @@ class Profile extends React.Component {
     this.state = {
       name: '',
       last_name: '',
-      email: ''
+      email: '',
+      pais: ''
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -30,14 +31,12 @@ class Profile extends React.Component {
   componentDidMount() {
 
     let url = 'http://127.0.0.1:8000/api/user/'+this.props.userId;
-    console.log(url);
     axios.get(url)
     .then((response) => {
-      console.log("Entre al then");
-      console.log(response.data);
       this.setState({name: response.data.first_name});
       this.setState({last_name: response.data.last_name});
       this.setState({email: response.data.email});
+      this.setState({pais: response.data.pais});
     })
     .catch(function (error) {
       // handle error
@@ -49,7 +48,12 @@ class Profile extends React.Component {
     e.preventDefault();
 
     let url = 'http://127.0.0.1:8000/api/update_user/'+this.props.userId;
-    const data = {"first_name": this.state.name, "last_name": this.state.last_name, "email": this.state.email};
+    const data = {
+      "first_name": this.state.name,
+      "last_name": this.state.last_name,
+      "email": this.state.email,
+      "pais": this.state.pais
+    };
     axios.put(url, data)
     .then(function (response) {
       window.location.reload();
@@ -61,48 +65,64 @@ class Profile extends React.Component {
 
   render(){
     return (
-      <div style={useStyles.root}>
-        <form noValidate onSubmit={e => this.handleSubmit(e)}>
-          <TextField
-            name='nombre'
-            label='Nombre'
-            type='text'
-            margin='normal'
-            fullWidth
-            placeholder='Nombre'
-            value={this.state.name}
-            onChange={(e) => this.setState({name:e.target.value})}
-          />
-          <TextField
-            name='apellido'
-            label='Apellido'
-            type='text'
-            margin='normal'
-            fullWidth
-            placeholder='Apellido'
-            value={this.state.last_name}
-            onChange={(e) => this.setState({last_name:e.target.value})}
-          />
-          <TextField
-            name='email'
-            label='Email'
-            type='text'
-            margin='normal'
-            fullWidth
-            placeholder='Email'
-            value={this.state.email}
-            onChange={(e) => this.setState({email:e.target.value})}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            style={useStyles.submit}
-          >
-            Guardar
-          </Button>
-        </form>
+      <div container style={useStyles.root}>
+        <div>
+          <form noValidate onSubmit={e => this.handleSubmit(e)}>
+            <div>
+              <TextField
+                name='nombre'
+                label='Nombre'
+                type='text'
+                margin='normal'
+                fullWidth
+                placeholder='Nombre'
+                value={this.state.name}
+                onChange={(e) => this.setState({name:e.target.value})}
+              />
+            </div>
+            <div>
+              <TextField
+                name='apellido'
+                label='Apellido'
+                type='text'
+                margin='normal'
+                fullWidth
+                placeholder='Apellido'
+                value={this.state.last_name}
+                onChange={(e) => this.setState({last_name:e.target.value})}
+              />
+            </div>
+            <TextField
+              name='email'
+              label='Email'
+              type='text'
+              margin='normal'
+              fullWidth
+              placeholder='Email'
+              value={this.state.email}
+              onChange={(e) => this.setState({email:e.target.value})}
+            />
+            <TextField
+              name='pais'
+              label='Pais'
+              type='text'
+              margin='normal'
+              fullWidth
+              placeholder='Pais'
+              value={this.state.pais}
+              onChange={(e) => this.setState({pais:e.target.value})}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              style={useStyles.submit}
+            >
+              Guardar
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
