24603e125a698b0dfbe4c35059a3976732272ff7
vicabitu <victor.ch370@gmail.com>
2020-02-03 13:37:17 -0300

    Pantalla de generacion de diplomas

--- frontend/generador_de_diplomas/src/components/GenerateDiplomas.js
+++ frontend/generador_de_diplomas/src/components/GenerateDiplomas.js
@@ -25,38 +25,6 @@ class GenerateDiplomas extends React.Component {
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.save = this.save.bind(this);
-  }
-
-  save(){
-    console.log("Estoy en el save");
-    console.log(this.state.fileZip);
-    // const blod = new Blob([this.state.fileZip], {type: "application/zip"});
-    // console.log(blod);
-    
-    // const url = window.URL.createObjectURL(blod);
-    // console.log(url);
-    // const a = document.createElement("a");
-    // a.href = url;
-    // a.download = 'diplomas.zip';
-    // a.click()
-
-    // var file;
-    // var data = [];
-    // data.push(this.state.fileZip);
-    // var properties = {type: 'application/octet-stream'}; // Specify the file's mime-type.
-    // try {
-    //   // Specify the filename using the File constructor, but ...
-    //   file = new File(data, "file.zip", properties);
-    // } catch (e) {
-    //   // ... fall back to the Blob constructor if that isn't supported.
-    //   file = new Blob(data, properties);
-    // }
-    // var url = URL.createObjectURL(file);
-    // document.getElementById('link').href = url;
-    
-    // var fileDownload = require('js-file-download');
-    // fileDownload(this.state.fileZip, 'diplomas.zip');
   }
 
   handleSubmit(e) {
@@ -76,14 +44,6 @@ class GenerateDiplomas extends React.Component {
       console.log("Entre al then");
       console.log(response);
 
-      // var binario = response.data;
-      // var name = 'diplomas.zip';
-
-      // this.setState({fileZip: binario});
-
-      // var fileDownload = require('js-file-download');
-      // fileDownload(response.data, 'diplomas.zip');
-      // window.location.reload('http://127.0.0.1:8000/media/diplomas/Generate_Diplomas.zip')
       window.location.replace("http://127.0.0.1:8000/media/diplomas/diplomas.zip")
     })
     .catch(function (error) {
@@ -93,11 +53,14 @@ class GenerateDiplomas extends React.Component {
   }
 
   render() {
+    var name_file = '';
+    this.state.file ? name_file = this.state.file.name : name_file = '';
+
     return (
       <div style={useStyles.root}>
         <form noValidate onSubmit={e => this.handleSubmit(e)}>
           <Typography component="h1" variant="h5">
-            Generar Diplomas.
+            Generar Diplomas
           </Typography>
 
           <input
@@ -113,6 +76,14 @@ class GenerateDiplomas extends React.Component {
             </Button>
           </label>
 
+          {/* Muestro el nombre del archivo que seleccionaron */}
+          {name_file &&
+            <div>
+              <p style={{fontWeight: 'bold'}}>Archivo seleccionado:</p>
+              <p style={{fontWeight: 'bold'}}>{name_file}</p>
+            </div>
+          }
+
           <Button 
             type="submit"
             fullWidth
@@ -123,18 +94,6 @@ class GenerateDiplomas extends React.Component {
             Generar
           </Button>
         </form>
-
-        <Button 
-            fullWidth
-            variant="contained"
-            color="primary"
-            style={useStyles.submit}
-            onClick={ () => this.save() }
-          >
-            Generar
-          </Button>
-
-          <a id="download_link" download="my_exported_file.txt" href='' >Download as Text File</a>
       </div>
     );
   }
