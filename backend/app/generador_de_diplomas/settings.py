5a30f25748ce03b5f514d2bc2d3e6d30952c5f6b
vicabitu <victor.ch370@gmail.com>
2020-02-06 15:34:30 -0300

    Cambio de tiempo de vencimiento del token

--- backend/app/generador_de_diplomas/settings.py
+++ backend/app/generador_de_diplomas/settings.py
@@ -143,8 +143,8 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=60)
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1)
 }
 
 MEDIA_URL = '/media/'
